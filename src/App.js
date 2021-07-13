77a7775435add0449ce38ced00e53e2e1853c564
abdulbytes <abdul.lezin57@gmail.com>
2021-07-13 12:29:13 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import { Container } from './Container';
+import { Filler } from './Filler';
+
+const App = () => {
+  const triggerText = 'Open form';
+  const onSubmit = (event) => {
+    event.preventDefault(event);
+    console.log(event.target.name.value);
+    console.log(event.target.email.value);
+  };
+  return (
+    <div className="App">
+      <Container triggerText={triggerText} onSubmit={onSubmit} />
+    </div>
+  );
+};
+
+export default App;
